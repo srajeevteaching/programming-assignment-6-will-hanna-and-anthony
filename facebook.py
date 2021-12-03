2e048931583ff1c81642dcbf4396cb109287216c
WillGrimmer <90355797+WillGrimmer@users.noreply.github.com>
2021-12-02 21:18:54 -0500

    Create facebook.py
    sheeeeeeeeesh

---
+++ facebook.py
@@ -0,0 +1,59 @@
+import numpy as np
+def read_file(filename):
+    data_list=[]
+    file=open(filename,"r")
+    for line in file:
+        data_list.append(line.split(";"))
+    file.close()
+    return data_list
+def fixlast(l):
+    for x in l:
+        x[-1] = x[-1].strip()
+    return l
+def filemaker(l,filename):
+    t=open(filename,'w')
+    for x in l:
+        t.write(str(x)+"\n")
+def difference(fb):
+    out=[]
+    for i in fb:
+        if float(i[16])>float(i[17]):
+            diff=float(i[16])-float(i[17])
+            los='likes'
+        else:
+            diff=float(i[17])>float(i[16])
+            los='share'
+        if diff <= 25:
+            change='no change'
+        elif diff <= 100:
+            change='more'
+        else:
+            change='significantly more'
+        a=[i[1],los,change]
+        out.append(a)
+    return out
+def type_likes(fb):
+    idk=[['Photo',0,0],['Link',0,0],['Video',0,0],['Status',0,0]]
+    for i in fb:
+        for x in idk:
+            if i[1]==x[0]:
+                x[1]+=1#times it happens
+                x[2]+=float(i[16])#total likes
+    for i in idk:
+        i[1]= i[2]/i[1]
+    for i in idk:
+        i.pop()
+        i[1]=round(i[1],2)
+    print(idk)
+
+
+def main():
+    Facebook_List=read_file('facebook.csv')
+    x=input("what would you like the filename to be?")
+    filename=str(x)+'.txt'
+    out=difference(Facebook_List)
+    filemaker(out,filename)
+    type_likes(Facebook_List)
+
+
+main()
