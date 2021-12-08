237d3f0189308c6a48c3c7fa5e504f18a17d4c2e
WillGrimmer <90355797+WillGrimmer@users.noreply.github.com>
2021-12-07 20:17:44 -0500

    Update facebook.py
    Finished

--- facebook.py
+++ facebook.py
@@ -4,6 +4,8 @@
 # Program Inputs: File name
 # Program Ouputs: Difference of likes and shares, average likes per type of post
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 def read_file(filename):
     data_list=[]
     file=open(filename,"r")
@@ -38,27 +40,81 @@ def difference(fb):
         out.append(a)
     return out
 def type_likes(fb):
-    idk=[['Photo',0,0],['Link',0,0],['Video',0,0],['Status',0,0]]
+    b=[['Photo',0,0],['Link',0,0],['Video',0,0],['Status',0,0]]
     for i in fb:
-        for x in idk:
+        for x in b:
             if i[1]==x[0]:
-                x[1]+=1
-                x[2]+=float(i[16])
-    for i in idk:
+                x[1]+=1#times it happens
+                x[2]+=float(i[16])#total likes
+    for i in b:
         i[1]= i[2]/i[1]
-    for i in idk:
+    for i in b:
         i.pop()
         i[1]=round(i[1],2)
-    print(idk)
+    print("The average likes for each type are...")
+    print(b)
+
+def make_list(fb):
+    a=[0,0,0]
+    for i in fb:
+        if float(i[9])<100:
+            a[0]+=1
+        if float(i[9])<400:
+            a[1]+=1
+        else:
+            a[2]+=1
+    return a
+def month_engage(m,fb):
+    x=[0]*12
+    val=0
+    c=0
+    for i in fb:
+        x[int((float(i[3])-1))]+=1
+    for i in x:
+        if i > val:
+            val=i
+            num=c
+        c+=1
+    print('The month with the highest engagement is...')
+    print(m.get(str(num)))
+
+
+
+def makegraph(a):
+    Sections=['Low','Medium','High']
+    xpos = np.arange(len(Sections))
+    plt.xticks(xpos,Sections)
+    plt.bar(xpos,a)
+    plt.title('Posts with Low, Medium and High Engagements ')
+    plt.ylabel('Number of Engagements')
+
+    plt.tight_layout()
+
+    plt.show()
 
 
 def main():
+    month = {'1': 'Janauary',
+             '2': 'February',
+             '3': 'March',
+             '4': 'April',
+             '5': 'May',
+             '6': 'June',
+             '7': 'July',
+             '8': 'August',
+             '9': 'September',
+             '10': 'October',
+             '11': 'November',
+             '12': 'December'	}
     Facebook_List=read_file('facebook.csv')
     x=input("what would you like the filename to be?")
     filename=str(x)+'.txt'
     out=difference(Facebook_List)
     filemaker(out,filename)
     type_likes(Facebook_List)
+    dalist=make_list(Facebook_List)
+    month_engage(month, Facebook_List)
+    makegraph(dalist)
 
 
 main()
